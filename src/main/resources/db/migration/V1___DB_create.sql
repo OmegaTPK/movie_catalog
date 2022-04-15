afe8cf52b63eed1d9d241f59d1b2fd86970d2048
Oleksii Sviatyshenko <oleksii.sviatyshenko@sigma.software>
2022-04-15 12:30:52 +0300

    complite servise ver 2

--- src/main/resources/db/migration/V1___DB_create.sql
+++ src/main/resources/db/migration/V1___DB_create.sql
@@ -1,3 +1,3 @@
-drop sequence if exists hibernate_sequence;
-create sequence hibernate_sequence start 1 increment 1;
-create table if not exists movie_entity (id int8 not null, description varchar(2000) not null, name varchar(255) not null, rate float8 not null, year timestamp not null, primary key (id));
+drop sequence if exists hibernate_sequence
+create sequence hibernate_sequence start 1 increment 1
+create table if not exists movie_entity (id int8 not null, description varchar(2000) not null, name varchar(255) not null, rate float8 not null, year timestamp not null, primary key (id))
