f3d1904f52935ea866bf0ec6c4eec34d0ea019d8
Oleksii <kolt07@meta.ua>
2022-05-06 20:02:46 +0300

    MOV-7_actors_movie_link (#8)
    * MOV-7_actors_movie_link
    
    * MOV-7_actors_movie_link
    
    * MOV-7_actors_movie_link
    
    * MOV-7_actors_movie_link
    
    Co-authored-by: Oleksii Sviatyshenko <oleksii.sviatyshenko@sigma.software>

--- src/main/java/com/example/moviecatalog/converter/ActorConverter.java
+++ src/main/java/com/example/moviecatalog/converter/ActorConverter.java
@@ -4,10 +4,13 @@ import com.example.moviecatalog.dto.ActorDto;
 import com.example.moviecatalog.entity.ActorEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
+
 @Component
 public class ActorConverter {
 
     public ActorDto convert(ActorEntity entity) {
+        //TODO make DTOs have a linked movies
         return new ActorDto(entity.getId(),
                 entity.getName(),
                 entity.getSurname(),
@@ -20,6 +23,7 @@ public class ActorConverter {
     }
 
     public ActorEntity convert(ActorDto dto) {
+        //TODO make DTOs have a linked movies
         return new ActorEntity(dto.getId(),
                 dto.getName(),
                 dto.getSurname(),
@@ -28,6 +32,18 @@ public class ActorConverter {
                 dto.getBirthDate(),
                 dto.getActiveStartDate(),
                 dto.getBirthPlace(),
-                dto.getDescription());
+                dto.getDescription(),
+                new HashSet<>());
+    }
+
+    public void fillEntityFromDto(ActorDto dto, ActorEntity entity) {
+        entity.setDescription(dto.getDescription());
+        entity.setName(dto.getName());
+        entity.setActiveStartDate(dto.getActiveStartDate());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setBirthPlace(dto.getBirthPlace());
+        entity.setGender(dto.getGender());
+        entity.setMiddleName(dto.getMiddleName());
+        entity.setSurname(dto.getSurname());
     }
 }
--- src/main/java/com/example/moviecatalog/service/MovieService.java
+++ src/main/java/com/example/moviecatalog/service/MovieService.java
@@ -1,8 +1,10 @@
 package com.example.moviecatalog.service;
 
 import com.example.moviecatalog.converter.MovieConverter;
+import com.example.moviecatalog.dao.ActorDao;
 import com.example.moviecatalog.dao.MovieDao;
 import com.example.moviecatalog.dto.MovieDto;
+import com.example.moviecatalog.entity.ActorEntity;
 import com.example.moviecatalog.entity.MovieEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
@@ -15,22 +17,26 @@ public class MovieService {
 
     private MovieDao movieDao;
     private MovieConverter movieConverter;
+    private ActorDao actorDao;
 
     @Autowired
-    public MovieService(MovieDao movieDao, MovieConverter movieConverter) {
+    public MovieService(MovieDao movieDao, MovieConverter movieConverter, ActorDao actorDao) {
         this.movieDao = movieDao;
         this.movieConverter = movieConverter;
+        this.actorDao = actorDao;
     }
 
     public List<MovieDto> getMovies() {
-        List<MovieEntity> entities = (List<MovieEntity>) movieDao.findAll();
+        List<MovieEntity> entities = movieDao.findAll();
         return movieConverter.convertEntities(entities);
     }
 
     public MovieDto addMovie(@NonNull MovieDto movieDto) {
         MovieEntity movieEntity = movieConverter.convert(movieDto);
+
         movieEntity.setId(null);
         movieEntity = movieDao.save(movieEntity);
+
         return movieConverter.convert(movieEntity);
     }
 
@@ -43,12 +49,26 @@ public class MovieService {
 
     public MovieDto updateMovie(MovieDto dto, Long id) {
         //TODO redo with exception usage throw if movie not exist
-        MovieDto result = null;
-        if (movieExistById(id)) {
-            MovieEntity entity = movieConverter.convert(dto);
-            entity.setId(id);
-            result = movieConverter.convert(movieDao.save(entity));
-        }
+        MovieDto result;
+        MovieEntity entity = movieDao.getById(id);
+
+        movieConverter.fillEntityFromDto(dto, entity);
+        result = movieConverter.convert(movieDao.save(entity));
+
+        return result;
+    }
+
+    public MovieDto addActorToMovie(Long movieId, Long actor_id) {
+        //TODO redo exception
+        MovieDto result;
+        MovieEntity movieEntity = movieDao.getById(movieId);
+        ActorEntity actorEntity = actorDao.getById(actor_id);
+
+        movieEntity.addActor(actorEntity);
+        movieEntity = movieDao.save(movieEntity);
+
+        result = movieConverter.convert(movieEntity);
+
         return result;
     }
 
