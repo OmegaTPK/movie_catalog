79d86c9dfea0ad96121a1049785fec97ab2ac7b2
Oleksii Sviatyshenko <oleksii.sviatyshenko@sigma.software>
2022-05-11 09:56:39 +0300

    MOV-9_lombok_add

--- src/main/java/com/example/moviecatalog/dto/ActorDto.java
+++ src/main/java/com/example/moviecatalog/dto/ActorDto.java
@@ -1,12 +1,14 @@
 package com.example.moviecatalog.dto;
 
 import com.example.moviecatalog.enums.Gender;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serializable;
 import java.time.Instant;
 
 @Data
+@AllArgsConstructor
 public class ActorDto implements Serializable {
     private Long id;
     private String name;
@@ -17,24 +19,4 @@ public class ActorDto implements Serializable {
     private Instant activeStartDate;
     private String birthPlace;
     private String description;
-
-    public ActorDto(Long id,
-                    String name,
-                    String surname,
-                    String middleName,
-                    Gender gender,
-                    Instant birthDate,
-                    Instant activeStartDate,
-                    String birthPlace,
-                    String description) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.middleName = middleName;
-        this.gender = gender;
-        this.birthDate = birthDate;
-        this.activeStartDate = activeStartDate;
-        this.birthPlace = birthPlace;
-        this.description = description;
-    }
 }
--- src/main/java/com/example/moviecatalog/dto/MovieDto.java
+++ src/main/java/com/example/moviecatalog/dto/MovieDto.java
@@ -1,23 +1,17 @@
 package com.example.moviecatalog.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serializable;
 import java.time.Instant;
 
 @Data
+@AllArgsConstructor
 public class MovieDto implements Serializable {
     private Long id;
     private String name;
     private Instant year;
     private String description;
     private Double rate;
-
-    public MovieDto(Long id, String name, Instant year, String description, Double rate) {
-        this.id = id;
-        this.name = name;
-        this.year = year;
-        this.description = description;
-        this.rate = rate;
-    }
 }
